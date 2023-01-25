a381592222946dbf21080eb2a132614e05c297a0
John Lai <lai.john.py@gmail.com>
2023-01-24 23:09:06 -0500

    Add current time to js if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  const todayObj = dayjs();
+  const currentTime = todayObj.format('H');
+  console.log(currentTime);
+
+  console.log(todayObj);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -23,12 +28,20 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $.each(timeBlocks, function (key, value) {
+    if ($(this).attr('id') === `hour-${currentTime}`) {
+      console.log(`this is the tiime`);
+      $(this).addClass('present');
+    }
+  });
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(todayObj.format('dddd - MMM  D, YYYY'));
 
+  ///////// add maybe
   //  if the time is passed disable the storage btn and text input
 });
